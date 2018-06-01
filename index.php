1e3577e94280b110153cd91c3076561999868ef8
jeremybleubanquise <39823264+jeremybleubanquise@users.noreply.github.com>
2018-06-01 15:04:51 +0200

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<?php include('accueil.php'); ?>
-<?php include('usine.php'); ?>
-<?php include('equipe.php'); ?>
-<?php include('real.php'); ?>
-<?php include('contact.php'); ?>
-<?php get_footer(); ?>
